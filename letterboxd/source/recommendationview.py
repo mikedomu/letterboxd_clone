8ea78b5bbdd9ee5a7ecd77e92df01b7bd08bbe02
mikedomu <125810597+mikedomu@users.noreply.github.com>
2025-06-09 21:26:49 +0200

    Delete letterboxd/source directory

--- letterboxd/source/recommendationview.py
+++
@@ -1,5 +0,0 @@
-import customtkinter
-class RecommendationView(customtkinter.CtkFrame):
-    def __init__(self, parent,controller):
-        super().__init__(parent)
-        self.controller = controller
